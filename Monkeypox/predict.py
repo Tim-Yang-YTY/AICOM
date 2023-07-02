9a8a8ccd7fdc7c7b18526d30de0a5e2f384b68bc
jcavenue <jcfababeir09@gmail.com>
2023-07-02 22:57:47 +0800

    Update predict

--- Monkeypox/predict.py
+++ Monkeypox/predict.py
@@ -2,7 +2,7 @@ import csv
 from test_automation import prediction
 
 results = []
-for i in range(3528, 3529):
+for i in range(1, 3529):
 	print(f"\033[1;32mImage testing_ds_others_img_{i}\033[0m")
 	imagePath = f"6124_testing_dataset/Others/testing_ds_others_img_{i}..jpg"
 	row = [f"Image testing_ds_others_img_{i}"]
